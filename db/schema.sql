2ed942af633df4a11dd66118c563caaac9e0832a
malcolmberry96 <malcolm.berry65@gmail.com>
2018-11-21 19:53:18 -0600

    database updates

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,8 @@ USE burgers_db;
 
 CREATE TABLE `burgers` (
     `id` Int ( 11 ) AUTO_INCREMENT NOT NULL,
-    `burger_name` VARCHAR (25) NOT NULL,
-    `devoured` BOOLEAN,
+    `burger_name` VARCHAR (255) NOT NULL,
+    `devoured` BOOLEAN DEFAULT false,
 
     PRIMARY KEY (`id`)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,8 @@
+USE burgers_db;
+
 INSERT INTO burgers (burger_name) VALUES ('DOUBLE BACON CHEESE BURGER');
 INSERT INTO burgers (burger_name) VALUES ('DOUBLE CHEESE BURGER');
 INSERT INTO burgers (burger_name) VALUES ('AVACADO BURGER');
 INSERT INTO burgers (burger_name) VALUES ('ANDREA BURGER');
+
 
